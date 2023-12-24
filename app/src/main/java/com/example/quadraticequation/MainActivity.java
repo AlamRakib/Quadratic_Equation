f2d4c8888bc8bc53d04fe81d83cb6c32c08ee4e8
shamsul-alam-rakib <shamsulalamrakib1@gmail.com>
2023-12-24 14:38:10 +0600

    This is my First Commit

---
+++ app/src/main/java/com/example/quadraticequation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.quadraticequation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.quadraticequation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        MyEquation equation = new MyEquation(mainBinding);
+
+        mainBinding.setMyequation(equation);
+
+    }
+
+
+
+
+}
